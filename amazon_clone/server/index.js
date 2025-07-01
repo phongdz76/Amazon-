959f8b4d2f56fa02fb39047a397abc17cd86c044
phongnguyen <145427852+phongdz76@users.noreply.github.com>
2025-07-02 03:15:10 +0700

    connection mongodb

--- amazon_clone/server/index.js
+++ amazon_clone/server/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
-const PORT = 3000;
+const authRouter = require('./routes/auth');
+const mognoose = require('mongoose');
 
+const PORT = 3000;
 const app = express();
+const DB = "mongodb+srv://phongnguyen07062004:Chauchau8@cluster0.6hzwav2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+app.use(express.json());
+app.use(authRouter);
+
+mognoose
+.connect(DB)
+.then(() => {
+    console.log("Connected to MongoDB");
+    })
+    .catch((e) => { 
+    console.error(e);
+}); 
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Connected to port ${PORT}`);
---
+++ amazon_clone/server/models/user.js
@@ -0,0 +1,39 @@
+const e = require('express');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String,
+        trim: true,
+    },
+    email: {
+        required: true,
+        type: String,
+        trim: true,
+        validate: {
+        validator: (value) => {
+        const re =
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return value.match(re);
+      },
+      message: "Please enter a valid email address",
+    },
+    },
+    password: {
+       required: true,
+       type: String,
+       minlength: 6,
+       maxlength: 80,
+    },
+    address: {
+       type: String,
+       default: "",
+    },
+    type: {
+       type: String,
+       default: "user",
+    },
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
